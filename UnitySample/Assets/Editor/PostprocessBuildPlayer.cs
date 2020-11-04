913de9193be99281c0b529364cf30de335291a66
조을연 <carpinus0@gmail.com>
2020-11-04 11:58:13 +0900

    Unity Sample Postprocess 추가 (ios13이하 대응)

---
+++ UnitySample/Assets/Editor/PostprocessBuildPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public static class PostprocessBuildPlayer 
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget buildTarget,string buildPath)
+    {
+#if UNITY_IOS
+        if(buildTarget== BuildTarget.iOS)
+        {
+            string projectPath = buildPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
+            PBXProject pBXProject = new PBXProject();
+            pBXProject.ReadFromFile(projectPath);
+
+            
+            var unityAppTargetID = pBXProject.GetUnityMainTargetGuid();
+
+            pBXProject.SetBuildProperty(unityAppTargetID, "ALWAYS_EMBEDED_SWIFT_STANDARD_LIBRARIES", "YES");
+           
+            pBXProject.WriteToFile(projectPath);
+        }
+#endif
+    }
+    
+}
